93b7f4786f8612f9ff5995e760ebf386690cdb9b
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-07-01 17:00:31 -0300

    Fixing nRun

--- examples/scenario102.cc
+++ examples/scenario102.cc
@@ -134,7 +134,7 @@ int main (int argc, char *argv[])
 
   cmd.Parse (argc, argv);
 
-  RngSeedManager::SetSeed (time (NULL));
+  //RngSeedManager::SetSeed (30);
   RngSeedManager::SetRun (nRun);
 
   // Set the EDs to require Data Rate control from the NS
